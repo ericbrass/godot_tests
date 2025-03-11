d0ab9cd7a7e44701f3e7bb1580a89c81a077c530
Eric Brass <ericbrass@gmail.com>
2025-03-11 08:33:02 -0400

    update_031120250832

--- game1_flappy/scripts/main_game.gd
+++ game1_flappy/scripts/main_game.gd
@@ -1,21 +1,32 @@
 extends Node2D
 
 @onready var animation_player = $player/Sprite2D/AnimationPlayer
-@onready var pipe = preload("res://scenes/pipe.tscn").instantiate()
+@onready var pipe = preload("res://scenes/pipe.tscn")
+
+var debug_counter :float = 0.0 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$pipeSpawnTimer.wait_time = 3.0
+	$pipeSpawnTimer.wait_time = 4.0
 	$pipeSpawnTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("tap"):
 		animation_player.play("jump")
+	debug_counter += delta
+	$debugLabel.text = str(debug_counter)
 
 func _on_world_border_body_entered(body: Node2D) -> void:
-	$player.queue_free()
-	$mainLabel.text = "Game Over"
+	game_over()
 
 func _on_pipe_spawn_timer_timeout() -> void:
-	$".".add_child(pipe)
+	var new_pipe = pipe.instantiate()
+	new_pipe.position.x = 550
+	new_pipe.position.y = randi_range(200,600)
+	add_child(new_pipe)
+
+func game_over():
+	$player.queue_free()
+	$mainLabel.text = "Game Over"
+	get_tree().paused = true
--- game1_flappy/scripts/pipe.gd
+++ game1_flappy/scripts/pipe.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+@onready var player: CharacterBody2D = %player
+@onready var main_label: Label = $mainLabel
+
+var pipe_speed = 100.0
+
 func _process(delta: float) -> void:
-	position.x -= 30
+	position.x -= pipe_speed * delta
 
 func _on_body_entered(body: Node2D) -> void:
-	print ("game over")
+	pass
